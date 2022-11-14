d90d8783c6c4549985020ca63d191258c1936309
Univaults ($UNVT) <113686571+Univaults@users.noreply.github.com>
2022-11-15 05:51:53 +0800

    Create master_ca.sol

---
+++ master_ca.sol
@@ -0,0 +1,3 @@
+// BSCSCAN: https://bscscan.com/address/0xc99688577554905d641889b43421d8cbac7980ec#code
+// CA     : 0xc99688577554905d641889B43421d8cBAC7980EC
+// All staked UNVT get sent to this address address for rewards.
